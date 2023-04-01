2ab7cdc98f48937b443c1e3fe3d91539ab2758af
Pepijn te Marvelde <p.h.temarvelde@student.tudelft.nl>
2023-04-01 20:46:13 +0200

    fix start script

--- start_amalur_container.sh
+++ start_amalur_container.sh
@@ -1,8 +1,8 @@
 for cores in 4 8 16 32 64
 do
     echo "Starting amalur parallel with $cores cores"
-    docker run --rm --name pepijn_amalur_parallel_cores_$cores --cpus="$cores.0" \
-      --env NUM_CORES=$cores --env NUM_REPEATS=30 EXPERIMENT_LOG_LEVEL=INFO\
+    docker run --rm -d --name pepijn_amalur_parallel_cores_$cores --cpus="$cores.0" \
+      --env NUM_CORES=$cores --env NUM_REPEATS=30 --envEXPERIMENT_LOG_LEVEL=INFO \
       -v /workspace/Pepijn/amalur_project/results/profile1:/user/src/app/amalur-factorization/results \
       ghcr.io/ptemarvelde/amalur-experiments:latest python profile_experiment.py
 done
