f7a86a1faae5de080767a58426589cc2cbe6aeae
Pepijn te Marvelde <ptemarvelde@gmail.com>
2023-05-12 13:49:04 +0200

    use pepijn/cupy branch

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/ADALabUCSD/MorpheusPy.git
-git clone -b experiments-pepijn https://github.com/delftdata/amalur-factorization.git
+git clone -b pepijn/cupy https://github.com/delftdata/amalur-factorization.git
 git clone -b experiments-pepijn https://github.com/delftdata/amalur-cost-estimation.git
 git clone https://github.com/flatironinstitute/sparse_dot.git
 git clone -b experiments-pepijn https://github.com/delftdata/amalur-data-generator.git
