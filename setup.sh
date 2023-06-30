def52c02a3c52143ba5ed86601f29b6347412026
Pepijn te Marvelde <ptemarvelde@gmail.com>
2023-06-30 13:58:37 +0200

    update costestimator branch

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/ADALabUCSD/MorpheusPy.git
 git clone -b pepijn/cupy https://github.com/delftdata/amalur-factorization.git
-git clone -b experiments-pepijn https://github.com/delftdata/amalur-cost-estimation.git
+git clone -b exp-sigmod https://github.com/delftdata/amalur-cost-estimation.git
 git clone https://github.com/flatironinstitute/sparse_dot.git
 git clone -b experiments-pepijn https://github.com/delftdata/amalur-data-generator.git
 #docker build . -t amalur_parallel
