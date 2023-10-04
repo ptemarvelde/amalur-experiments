b58d1b52f75d5b132cbd3d4dadde6429ecd909b4
Pepijn te Marvelde <ptemarvelde@gmail.com>
2023-10-04 21:34:48 +0200

    update daic dockerfile and script

--- daic_apptainer_build.sh
+++ daic_apptainer_build.sh
@@ -3,6 +3,8 @@ docker build -f Dockerfile.daic -t amalur:daic .
 dir=$(pwd)
 cd ~/Documents/uni/y5/thesis/daic
 apptainer build amalur_daic.sif docker-daemon://amalur:daic
+du -sh ./*
 echo "Syncing images dir"
+paplay /usr/share/sounds/sound-icons/prompt.wav
 rsync -rvh ~/Documents/uni/y5/thesis/daic/ daic:/tudelft.net/staff-umbrella/ThesisPepijnFactorizedML/images/
 cd $dir
