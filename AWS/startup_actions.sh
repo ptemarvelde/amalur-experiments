9cd0044a4f9b1ca2fe751e7c8f9f88bcda0e424e
Pepijn te Marvelde <ptemarvelde@gmail.com>
2023-10-15 16:58:14 +0200

    Start AWS startup script

--- AWS/startup_actions.sh
+++ AWS/startup_actions.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+GITHUB_CONTAINER_PAT=
+BUCKET_NAME=
+BUCKET_MOUNTPOINT=
+S3_KEY="<access_key>:<secret_key>"
+S3_KEYFILE=/etc/.passwd-s3fs
+
 # Select CUDA 12.1
 export PATH=/usr/local/cuda/bin/:/usr/local/cuda/include:$PATH
 sudo rm /usr/local/cuda
 sudo ln -s /usr/local/cuda-12.1 /usr/local/cuda
 
 # Pull experiments image from github container registry
-export CR_PAT="<>"
-echo $CR_PAT | docker login ghcr.io -u ptemarvelde --password-stdin
+echo "$GITHUB_CONTAINER_PAT" | docker login ghcr.io -u ptemarvelde --password-stdin
 docker pull ghcr.io/ptemarvelde/amalur-cupy:latest &
 
 # Mount Datasets using S3fs-Fuse
-BUCKET_NAME=
-BUCKET_MOUNTPOINT=
-S3_KEY="<access_key>:<secret_key>"
-S3_KEYFILE=/etc/.passwd-s3fs
 
 apt update
 apt install -y s3fs
