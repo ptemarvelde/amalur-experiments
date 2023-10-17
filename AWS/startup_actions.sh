5f9118ed2e8c66fc05e2785df11798c3a426fafc
Pepijn te Marvelde <ptemarvelde@gmail.com>
2023-10-17 11:21:16 +0200

    Update startup_actions

--- AWS/startup_actions.sh
+++ AWS/startup_actions.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 GITHUB_CONTAINER_PAT=
-BUCKET_NAME=
-BUCKET_MOUNTPOINT=
-S3_KEY="<access_key>:<secret_key>"
-S3_KEYFILE=/etc/.passwd-s3fs
+BUCKET_NAME=pepijn-test-bucket
+BUCKET_MOUNTPOINT=${HOME}/data
 
 # Select CUDA 12.1
 export PATH=/usr/local/cuda/bin/:/usr/local/cuda/include:$PATH
@@ -12,14 +9,12 @@ sudo rm /usr/local/cuda
 sudo ln -s /usr/local/cuda-12.1 /usr/local/cuda
 
 # Pull experiments image from github container registry
+GITHUB_CONTAINER_PAT=$(aws secretsmanager get-secret-value --region eu-central-1 --secret-id pepijn-test-gpu --query SecretString --output text | jq -r .ptemarvelde_github_container_pat)
 echo "$GITHUB_CONTAINER_PAT" | docker login ghcr.io -u ptemarvelde --password-stdin
 docker pull ghcr.io/ptemarvelde/amalur-cupy:latest &
 
 # Mount Datasets using S3fs-Fuse
-
-apt update
-apt install -y s3fs
-echo $S3_KEY> $S3_KEYFILE
-chmod 600 $S3_KEYFILE
-
-s3fs "$BUCKET_NAME" "$BUCKET_MOUNTPOINT" -o passwd_file=$S3_KEYFILE
+sudo apt update
+sudo apt install -y s3fs
+mkdir -p "$BUCKET_MOUNTPOINT"
+s3fs "$BUCKET_NAME" "$BUCKET_MOUNTPOINT" -o iam_role="auto"
