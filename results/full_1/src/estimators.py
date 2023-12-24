45485d2d76ae00974222e224a7fa0f08e7b06c86
Pepijn te Marvelde <ptemarvelde@gmail.com>
2023-12-24 16:07:02 +0100

    analysis edits and added notebook for icde fig6 flow

--- results/full_1/src/estimators.py
+++ results/full_1/src/estimators.py
@@ -64,32 +64,51 @@ class Amalur(Classifier):
 
 class MorpheusFI(Classifier):
     def __init__(self):
-        # Number of base tables with sparsity < 5% q, number of base
-        # tables p, sparsity of Ri ei, number of samples in S nS , number of rows in Ri ni.
         super().__init__(
             [
-                "morpheusfi_q",
-                "morpheusfi_p",
-                "morpheusfi_eis",
-                "morpheusfi_ns",
-                "morpheusfi_nis",
+                "morpheusfi_q",     # Number of base tables with sparsity < 5%
+                "morpheusfi_p",     # Number of base tables
+                "morpheusfi_eis",   # List: Sparsity of Ri
+                "morpheusfi_ns",    # Number of samples in S
+                "morpheusfi_nis",   # List: Number of rows in Ri
             ]
         )
+    
+    def optimize(self, q: int, p: int, eis: List[float], ns: int, nis: List[int]) -> bool:
+        """Morpheus FI Heuristic decision rule. Choos factorization if this returns True.
+        
+        In natural language:
+        Choose factorization if either
+            - The number of sparse base tables (q) is less than half of the total number of base tables (p)
+            - For all dimension tables i the sparsity of Ri times the number of rows in S divided by the number of rows in Ri is greater than 1.
+                - If all dim tables are fairly dense and relatively small (compared to Fact table), then factorization is better.
+        
+        S: Fact table
+        Ri: Dimension table i feature matrix
 
-    def optimize(self, q: int, p: int, eis: List[float], ns: int, nis: List[int]):
+        Args:
+            q (int): number of base tables with sparsity < 5%
+            p (int): number of base tables
+            eis (List[float]): sparsity of Ri
+            ns (int): number of rows in S
+            nis (List[int]): number of rows in Ri
+
+        Returns:
+            bool: Choose factorization (True) or materialization (False)
+        """
         return q < math.floor(p / 2) or (
             math.floor(q >= p / 2) and all([ei * (ns / ni) > 1 for (ei, ni) in zip(eis, nis)])
         )
 
-def eval_model(model, X_test, y_test, speedup=None):
+def eval_model(model, X_test, y_test, speedup=None, plot=False):
     print(f"Model {model.__class__}, test cols: {X_test.columns}")
     y_pred = model.predict(X_test)
-    result, fig = eval_result(y_test, y_pred=y_pred,speedup=speedup, model_name=model.__class__.__name__)
-    if not fig:
+    result, fig = eval_result(y_test, y_pred=y_pred,speedup=speedup, model_name=model.__class__.__name__, plot=plot)
+    if not fig and plot:
         fig = ConfusionMatrixDisplay.from_estimator(model, X_test, y_test, cmap="bone", text_kw={"size": 20})
     return result, fig
 
-def eval_result(y_test, y_pred, speedup=None, model_name=''):
+def eval_result(y_test, y_pred, speedup=None, model_name='', plot=False):
     y_true = y_test.copy()
 
     scoring_functions = {
@@ -101,7 +120,6 @@ def eval_result(y_test, y_pred, speedup=None, model_name=''):
     res = {}
     for name, function in scoring_functions.items():
         res[name] = function(y_true, y_pred)
-        print(f"Test {name} Score: {res[name]}")
 
     fig = None
     if speedup is not None:
@@ -154,13 +172,15 @@ def eval_result(y_test, y_pred, speedup=None, model_name=''):
         labels = np.asarray(
             [f"{v1}\n{v3}\n{v4}" for v1, v2, v3, v4 in zip(group_names, group_counts, group_percentages, group_spdup)]
         ).reshape(2, 2)
-        fig, axes = plt.subplots(1, 1, sharex=True, sharey=True, figsize=(5, 4.2))
-        fig.suptitle(model_name)
-        axes.set_title(f"Realized speedup of positive samples: {speedup_dict['tot_realized_speedup']:.2f}")
-        sns.heatmap(cf, annot=labels, cmap="Blues", fmt="", ax=axes, cbar=True)
-        axes.set_yticklabels(["Materialize", "Factorize"], rotation=90)
-        axes.set_xticklabels(["Materialize", "Factorize"])
-        axes.set_xlabel("Predicted label")
-        axes.set_ylabel("True label")
+        fig = None
+        if plot:
+            fig, axes = plt.subplots(1, 1, sharex=True, sharey=True, figsize=(5, 4.2))
+            fig.suptitle(model_name)
+            axes.set_title(f"Realized speedup of positive samples: {speedup_dict['tot_realized_speedup']:.2f}")
+            sns.heatmap(cf, annot=labels, cmap="Blues", fmt="", ax=axes, cbar=True)
+            axes.set_yticklabels(["Materialize", "Factorize"], rotation=90)
+            axes.set_xticklabels(["Materialize", "Factorize"])
+            axes.set_xlabel("Predicted label")
+            axes.set_ylabel("True label")
 
     return res, fig
